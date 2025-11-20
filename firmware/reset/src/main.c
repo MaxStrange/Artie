3a208680cf16b47cd866ec5221293d0b619b11a5
Max Strange <maxfieldstrange@gmail.com>
2025-11-20 10:59:36 -0800

    WIP: Reset

--- firmware/reset/src/main.c
+++ firmware/reset/src/main.c
@@ -3,12 +3,44 @@
 #include <stdio.h>
 // SDK includes
 #include "pico/stdlib.h"
+// Library includes
+#include <leds.h>
+#include <errors.h>
 // Local includes
+#include "board/pinconfig.h"
+#include "board/types.h"
+
+/**
+ * @brief Command table for the LED subsystem.
+ *
+ * @param command
+ */
+static void leds_cmd(cmd_t command)
+{
+    switch (command)
+    {
+        case CMD_LED_ON:
+            leds_on();
+            break;
+        case CMD_LED_OFF:
+            leds_off();
+            break;
+        case CMD_LED_HEARTBEAT:
+            leds_heartbeat();
+            break;
+        default:
+            log_error("Illegal cmd type 0x%02X\n in LED subsystem", command);
+            break;
+    }
+}
 
 int main()
 {
     // Initialize UART for debugging (in a release build, this should be turned off from the CMake build system)
     stdio_init_all();
+
+    // Initialize GPIO pins for LEDs
+    leds_init(LED_PIN);
 
     while (true)
     {
