c416aa010231f5dbe1a2aa1a2f18953a42d96fc9
Max Strange <maxfieldstrange@gmail.com>
2025-12-01 19:56:29 -0800

    WIP: Pipeline

--- framework/workbench/gui/widgets/experiment_tab.py
+++ framework/workbench/gui/widgets/experiment_tab.py
@@ -2,14 +2,30 @@
 Experiment tab widget for Artie Workbench
 """
 from PyQt6 import QtWidgets
+from model import artie_profile
+from model import settings
 
 
 class ExperimentTab(QtWidgets.QWidget):
     """Experiment progress tab showing schedule and controls"""
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent, settings: settings.WorkbenchSettings, profile: artie_profile.ArtieProfile):
+        super().__init__(parent)
+        self.settings = settings
+        self.profile = profile
         self._setup_ui()
+
+        # Connect to parent signals
+        self.parent().profile_switched_signal.connect(self.on_profile_switched)
+        self.parent().settings_changed_signal.connect(self.on_settings_changed)
+
+    def on_profile_switched(self, profile: artie_profile.ArtieProfile):
+        """Handle profile switch events"""
+        self.profile = profile
+
+    def on_settings_changed(self, settings: settings.WorkbenchSettings):
+        """Handle settings change events"""
+        self.settings = settings
     
     def _setup_ui(self):
         """Setup the experiment tab UI"""
