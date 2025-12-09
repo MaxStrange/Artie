32005d81fbbd51b860d060f06231d14f8287dfdb
Max Strange <maxfieldstrange@gmail.com>
2025-12-09 10:12:55 -0800

    WIP: Workbench

--- framework/workbench/gui/widgets/logging_tab.py
+++ framework/workbench/gui/widgets/logging_tab.py
@@ -1,5 +1,27 @@
 """
 Logging tab widget for Artie Workbench
+
+TODO:
+
+* The logging mechanics in Artie work like this:
+    - There is a Fluent Bit pod, which collects logs and currently just pushes them to stdout.
+      The fluent bit pod accepts logs pushed to it from each other pod via Artie logging library.
+    - There is a Prometheus pod, which collects metrics and currently stores them in /data inside
+      the Docker container (we need to update the Helm chart to actually map a physical drive to this directory).
+      Prometheus reaches out to each pod, which expose their metrics via a server thread, and scrapes them
+      periodically. Prometheus has a very nice metrics viewer API for collecting and displaying the information.
+* TODO: Update fluent bit configuration to store logs to disk
+* TODO: Update fluent bit configuration to allow API server to query the logs
+* TODO: Update pod configuration for fluent bit pod to mount a directory
+* TODO: Update pod configuration for prometheus pod to mount a directory
+* TODO: Require a compute node for at least log storing purposes
+* TODO: Update Artie API Server to include an API for retrieving all logs from within the past X seconds
+* TODO: Update Artie API Server to include an API for querying logs
+* TODO: Update Artie API Server to include a set of HTTP methods for querying metrics (see the Prometheus documentation for examples)
+* TODO: Update this file to make use of Artie API Server for log querying and "live" streaming.
+        The interface to the API server might make sense as a separate file in the comms directory, but not sure.
+* TODO: Add another widget to the main window, similar to this tab, but instead it should be for metrics.
+
 """
 from PyQt6 import QtWidgets
 from model import artie_profile
