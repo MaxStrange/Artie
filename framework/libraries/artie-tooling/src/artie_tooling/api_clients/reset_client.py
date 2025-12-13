403fbd7a55f631ecc4ff987cb3de79a347145498
Max Strange <maxfieldstrange@gmail.com>
2025-12-13 10:08:11 -0800

    WIP: Workbench

--- framework/libraries/artie-tooling/src/artie_tooling/api_clients/reset_client.py
+++ framework/libraries/artie-tooling/src/artie_tooling/api_clients/reset_client.py
@@ -1,9 +1,23 @@
 """
 Reset API client for communicating with the API Server's reset endpoints.
 
+Each `*Response` object corresponds to the response from a specific API endpoint.
+
 See [API documentation](../../../../../misc-micro-services/artie-api-server/README.md) for more details.
 """
 from . import api_client
+from .. import errors
+import dataclasses
+import enum
+
+@dataclasses.dataclass
+class StatusResponse:
+    """Response object for status request."""
+    artie_id: str
+    """The Artie ID."""
+
+    mcu_status: api_client.ModuleStatus
+    """The reset status."""
 
 class ResetClient(api_client.APIClient):
     def __init__(self, *args, **kwargs) -> None:
@@ -24,18 +38,24 @@ class ResetClient(api_client.APIClient):
             case _:
                 raise ValueError(f"Invalid argument given for MCU address: {address}")
 
-    def reset_target(self, address: int):
+    def reset_target(self, address: int) -> errors.HTTPError|None:
         mcu = self._convert_address_to_mcu(address)
-        self.post(f"/reset/mcu", params={'artie-id': self.artie_id, 'id': mcu})
+        response = self.post(f"/reset/mcu", params={'artie-id': self.artie_id, 'id': mcu})
+        if response.status_code != 200:
+            return errors.HTTPError(response.status_code, f"Error resetting target: {response.content.decode('utf-8')}")
+        return None
 
-    def status(self):
+    def status(self) -> StatusResponse|errors.HTTPError:
         response = self.get("/reset/status", params={'artie-id': self.artie_id})
         if response.status_code != 200:
-            common.format_print_status_result(f"Error getting reset status: {response.content.decode('utf-8')}", module='reset', artie_id=self.artie_id)
-        else:
-            common.format_print_status_result(response.json(), module='reset', artie_id=self.artie_id)
+            return errors.HTTPError(response.status_code, f"Error getting reset status: {response.content.decode('utf-8')}")
+        return StatusResponse(
+            artie_id=response.json().get('artie-id'),
+            mcu_status=api_client.ModuleStatus(response.json().get('submodule-statuses').get('MCU'))
+        )
 
-    def self_check(self):
+    def self_check(self) -> errors.HTTPError|None:
         response = self.post("/reset/self-test", params={'artie-id': self.artie_id})
         if response.status_code != 200:
-            common.format_print_result(f"Error running reset self-check: {response.content.decode('utf-8')}", module='reset', submodule='status', artie_id=self.artie_id)
+            return errors.HTTPError(response.status_code, f"Error running reset self-check: {response.content.decode('utf-8')}")
+        return None
