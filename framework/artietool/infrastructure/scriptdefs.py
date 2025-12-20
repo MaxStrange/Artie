24ecab01dbd7543902c68b651d9136fedaa6f84d
Max Strange <maxfieldstrange@gmail.com>
2025-12-19 21:34:38 -0800

    WIP: Yocto

--- framework/artietool/infrastructure/scriptdefs.py
+++ framework/artietool/infrastructure/scriptdefs.py
@@ -5,7 +5,9 @@ from dataclasses import dataclass
 from typing import List
 from .. import common
 import argparse
+import os
 import subprocess
+import tempfile
 
 @dataclass
 class ScriptDefinition:
@@ -14,32 +16,38 @@ class ScriptDefinition:
     """
     script_path: str|None = None
     inline_script: str|None = None
-    args: List[str]|None = None
+    args: List[str|dict]|None = None
 
     def is_inline(self) -> bool:
         """
         Returns True if this script definition is an inline script.
         """
         return self.inline_script is not None
-    
+
     def is_file(self) -> bool:
         """
         Returns True if this script definition is a script from a file.
         """
         return self.script_path is not None
-    
+
     def fill_in_args(self, args_obj) -> None:
         """
         Fills in the arguments for this script definition using the provided args object.
         """
         if self.args is None:
             return
-        
+
         filled_args = []
         for arg in self.args:
-            filled_arg = common.replace_variables(arg, args_obj)
-            filled_args.append(filled_arg)
-        
+            if isinstance(arg, dict):
+                for key, value in arg.items():
+                    filled_key = common.replace_vars_in_string(key, args_obj)
+                    filled_value = common.replace_vars_in_string(value, args_obj)
+                    filled_args.append({filled_key: filled_value})
+            else:
+                filled_arg = common.replace_vars_in_string(arg, args_obj)
+                filled_args.append(filled_arg)
+
         self.args = filled_args
 
     def run_script(self, runtime_args: argparse.Namespace, *args, **kwargs) -> subprocess.CompletedProcess:
@@ -50,11 +58,49 @@ class ScriptDefinition:
         First fills in the script definition with the provided runtime arguments via variable substitution.
         """
         if self.is_inline():
-            cmd = common.replace_variables(self.inline_script, runtime_args)
+            cmd = common.replace_vars_in_string(self.inline_script, runtime_args)
+            return self._run_inline_script(cmd, *args, **kwargs)
         elif self.is_file():
-            script_path = self.script_path
-            cmd = [script_path] + [arg for arg in self.args] if self.args else [script_path]
+            return self._run_file_script(self.script_path, *args, **kwargs)
         else:
             raise ValueError("ScriptDefinition must have either inline_script or script_path defined.")
 
-        return subprocess.run(cmd, *args, **kwargs)
+    def _run_inline_script(self, script: str, *args, **kwargs) -> subprocess.CompletedProcess:
+        """
+        Run the inline script by writing it to a temporary file and executing it along with any args.
+        """
+        # Create a temporary file to hold the script
+        with tempfile.NamedTemporaryFile(mode='w+', delete=True, delete_on_close=False, suffix='.sh') as temp_script:
+            temp_script.write(script)
+            temp_script.flush()
+            temp_script.close()
+            fpath = temp_script.name
+
+            # Make the script executable
+            os.chmod(fpath, 0o755)
+
+            # Now run it
+            return self._run_file_script(fpath, *args, **kwargs)
+
+    def _run_file_script(self, fpath: str, *args, **kwargs) -> subprocess.CompletedProcess:
+        """
+        Run the script from file along with any potential args we have.
+        """
+        cmd = [fpath]
+
+        if self.args:
+            # Add positional args
+            for arg in self.args:
+                if not isinstance(arg, dict):
+                    cmd.append(arg)
+            # Add key:value args
+            for arg in self.args:
+                if isinstance(arg, dict):
+                    for key, value in arg.items():
+                        cmd.append(f"{key}={value}")
+        try:
+            return subprocess.run(cmd, *args, **kwargs)
+        except OSError:
+            # Possibly the file needs to be run under a shell
+            cmd = ['/bin/bash'] + cmd
+            return subprocess.run(cmd, *args, **kwargs)
--- framework/artietool/infrastructure/yocto_build_job.py
+++ framework/artietool/infrastructure/yocto_build_job.py
@@ -53,7 +53,7 @@ class YoctoBuildJob(job.Job):
             args.repo_branch = 'main'
 
         # Now that we have runtime arguments, we can replace variables in places that need it
-        self.binary_fname = common.replace_variables(self.binary_fname, args)
+        self.binary_fname = common.replace_vars_in_string(self.binary_fname, args)
         self.setup_script.fill_in_args(args)
         self.build_cmd.fill_in_args(args)
         self.post_script.fill_in_args(args)
@@ -75,40 +75,49 @@ class YoctoBuildJob(job.Job):
         elif args.skip_clone and os.path.exists(git_repo_location):
             common.info(f"Skipping cloning of repo {self.repo} because '--skip-clone' was given and repository exists at {git_repo_location}.")
             common.info(f"Checking if the existing repo is on branch {args.repo_branch}...")
-            current_branch = subprocess.run(f"git branch --show-current", cwd=git_repo_location, capture_output=True, text=True, check=True).stdout.strip()
+            current_branch = subprocess.run(["git", "branch", "--show-current"], cwd=git_repo_location, capture_output=True, text=True, check=True).stdout.strip()
             if current_branch != args.repo_branch:
                 e = ValueError(f"Existing repo at {git_repo_location} is on branch '{current_branch}', but expected branch '{args.repo_branch}' Backing off to avoid potentially overwriting data.")
                 return result.JobResult(name=self.name, success=False, error=e)
         else:
             common.info(f"Cloning repo {self.repo} (branch {args.repo_branch}) into {git_repo_location}...")
-            subprocess.run(f"git clone {self.repo} -o {git_repo_location} -b {args.repo_branch}").check_returncode()
+            subprocess.run([f"git", "clone", self.repo, "-b", args.repo_branch, git_repo_location]).check_returncode()
 
         # Now, clone all the layers
         for layer in self.layers:
+            if os.path.exists(os.path.join(git_repo_location, layer.name)) and args.skip_clone:
+                common.info(f"Yocto layer {layer.name} already exists at {git_repo_location}/{layer.name}, skipping clone.")
+                continue
             layer_path = os.path.join(git_repo_location, layer.name)
             common.info(f"Cloning Yocto layer {layer.name} from {layer.url} into {layer_path}...")
-            cmd = f"git clone {layer.url} -o {layer_path}"
-            if layer.tag is not None:
-                cmd += f" -t {layer.tag} -b {layer.tag}-local"
+            # Clone the layer
+            cmd = [f"git", "clone", layer.url, layer_path]
             subprocess.run(cmd).check_returncode()
+            # Checkout the tag if specified
+            if layer.tag is not None:
+                common.info(f"Checking out tag {layer.tag} for layer {layer.name}...")
+                subprocess.run([f"git", "checkout", "-t", f"origin/{layer.tag}"], cwd=layer_path).check_returncode()
+            # Now create our own branch for modifications
+            subprocess.run([f"git", "checkout", "-b", f"{layer.tag}-local"], cwd=layer_path).check_returncode()
+
 
         # Run the setup script
         common.info(f"Running Yocto setup script...")
-        setup_result = self.setup_script.run_script(cwd=git_repo_location, extra_args=[])
-        if not setup_result.success:
-            return result.JobResult(name=self.name, success=False, error=setup_result.error)
-        
+        setup_result = self.setup_script.run_script(args, cwd=git_repo_location)
+        if not setup_result.returncode == 0:
+            return result.JobResult(name=self.name, success=False, error=OSError(f"Yocto setup script failed with return code {setup_result.returncode}. Stdout: {setup_result.stdout}; Stderr: {setup_result.stderr}."))
+
         # Run the build command
         common.info(f"Running Yocto build command...")
-        build_result = self.build_cmd.run_script(cwd=git_repo_location, extra_args=[f"YOCTO_IMAGE={args.yocto_image}"])
-        if not build_result.success:
-            return result.JobResult(name=self.name, success=False, error=build_result.error)
-        
+        build_result = self.build_cmd.run_script(args, cwd=git_repo_location)
+        if not build_result.returncode == 0:
+            return result.JobResult(name=self.name, success=False, error=OSError(f"Yocto build command failed with return code {build_result.returncode}. Stdout: {build_result.stdout}; Stderr: {build_result.stderr}."))
+
         # Run the post-build script
         common.info(f"Running Yocto post-build script...")
-        post_result = self.post_script.run_script(cwd=git_repo_location, extra_args=[f"image={args.yocto_image}"])
-        if not post_result.success:
-            return result.JobResult(name=self.name, success=False, error=post_result.error)
+        post_result = self.post_script.run_script(args, cwd=git_repo_location)
+        if not post_result.returncode == 0:
+            return result.JobResult(name=self.name, success=False, error=OSError(f"Yocto post-build script failed with return code {post_result.returncode}. Stdout: {post_result.stdout}; Stderr: {post_result.stderr}."))
 
         # Fill in the artifacts
         self.mark_all_artifacts_as_built()
