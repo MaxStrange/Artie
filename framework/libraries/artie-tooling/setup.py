5b22340fafd2860c6a78a2dad8aba355828ed280
Max Strange <maxfieldstrange@gmail.com>
2025-12-23 16:50:14 -0800

    Deal with Python packaging crap

--- framework/libraries/artie-tooling/setup.py
+++ framework/libraries/artie-tooling/setup.py
@@ -4,8 +4,14 @@ setup(
     version="0.0.1",
     python_requires=">=3.10",
     license="MIT",
-    packages=["artie_tooling"],
-    package_dir={"artie_tooling": "src/artie_tooling"},
+    packages=[
+        "artie_tooling",
+        "artie_tooling.api_clients",
+    ],
+    package_dir={
+        "artie_tooling": "src/artie_tooling",
+        "artie_tooling.api_clients": "src/artie_tooling/api_clients",
+    },
     install_requires=[
         "pyyaml>=6.0"
     ]
